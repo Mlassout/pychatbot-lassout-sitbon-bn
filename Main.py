28c97ee0f9a349e46d0c4ebe2fe2fdd708b77251
marc-antoine <marc-antoine.lassout@efrei.net>
2023-11-26 23:42:52 +0100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,188 @@
+from function import *
+
+if __name__=="__main__":
+
+
+# Call of the function
+    directory = "./speeches"
+    files_names = list_of_files(directory, "txt")
+
+    last_list = new_list(files_names)
+    last_list_colonne=sorted(last_list)
+    liste_president = []
+    liste_president = supp_doublons(last_list)
+
+
+
+    cleaned(files_names)
+
+    dictionnaire_mot = {}
+    directory_cleaned = "./cleaned"
+    dictionnaire_mot = cpt_word(directory_cleaned)
+
+
+    dico_idf = score_idf_dico(dictionnaire_mot)
+
+    Matrice = []
+
+    Matrice = matrice_tfidf(directory_cleaned)
+
+
+
+
+    mot_pas_important=[]
+    somme_tfidf=0
+    for j in range(2,len(Matrice)):
+        TF_IDF=int(Matrice[j][1])
+        if TF_IDF==0:
+            somme_tfidf=0
+            for k in range(2,9):
+                somme_tfidf+=int(Matrice[j][k])
+            if somme_tfidf==0:
+
+                mot_pas_important.append(Matrice[j][0])
+
+    indice_high_idf=0
+    for i in range(2, len(Matrice)-1):
+        for j in range (2,9):
+            if int(Matrice[i][j])<int(Matrice[i+1][j]):
+                indice_high_idf=i+1
+
+    mot_import_dossier=Matrice[indice_high_idf][0]
+
+
+    list_indice_mot=[]
+
+    for i in range(2, len(Matrice) - 1):
+        for j in range(2, 9):
+            if int(Matrice[i][j]) < int(Matrice[i + 1][j]):
+                indice_high_idf = i+1
+        list_indice_mot.append(indice_high_idf)
+
+
+    if list_indice_mot[0]<list_indice_mot[1]:
+        mot_import_chirac=Matrice[list_indice_mot[1]] [0]
+    else:
+        mot_import_chirac = Matrice[list_indice_mot[0]][0]
+
+
+    president_nation=[]
+    indice_mot_nation=0
+    for i in range (len(Matrice)):
+        if Matrice[i][0]=="nation":
+            indice_mot_nation=i
+    find_Chirac=False
+    find_Mitterrand=False
+    for i in range (2,9):
+        if Matrice[indice_mot_nation][i]>0:
+            if i==1 or i==2 and find_Chirac==False:
+
+                find_Chirac=True
+                president_nation.append("Chirac")
+            if i==3:
+
+                president_nation.append("Giscard")
+            if i==4:
+
+                president_nation.append("Hollande")
+            if i==5:
+
+                president_nation.append("Macron")
+            if i==6 or i==7 and find_Mitterrand==False:
+
+                find_Mitterrand=True
+                president_nation.append("Mitterrand")
+            if i==8:
+
+                president_nation.append("Sarkozy")
+
+    indice_high_idf=0
+    for i in range (2,8):
+        if Matrice[indice_mot_nation][i]<Matrice[indice_mot_nation][i+1]:
+            indice_high_idf=i+1
+        else:
+            indice_high_idf=i
+
+    if indice_high_idf == 1 or indice_high_idf == 2:
+        presi_nation="Chirac"
+    if indice_high_idf == 3:
+        presi_nation = "Giscard"
+    if indice_high_idf == 4:
+        presi_nation = "Hollande"
+    if indice_high_idf == 5:
+        presi_nation = "Macron"
+    if indice_high_idf == 6 or indice_high_idf == 7:
+        presi_nation = "Mitterrand"
+    if indice_high_idf == 8:
+        presi_nation = "Sarkozy"
+
+
+    mot_sans_non_important=[]
+    for i in range (1,len(Matrice)):
+        if Matrice[i][0] not in mot_pas_important:
+            mot_sans_non_important.append(Matrice[i][0])
+
+    indice_ecologie=0
+    for i in range (len(Matrice)):
+        if Matrice[i][0]=="écologie":
+            indice_ecologie=i
+
+    indice_ecologie_president=0
+    for j in range (2,8):
+        if Matrice[indice_ecologie][j]<Matrice[indice_ecologie][j+1]:
+            indice_ecologie_president=j
+        else:
+            indice_ecologie_president=j
+
+    president_ecolo=""
+    if indice_ecologie_president==1 or indice_ecologie_president==2 :
+        president_ecolo="Chirac"
+    if indice_ecologie_president==3:
+        president_ecolo = "Giscard"
+    if indice_ecologie_president == 4:
+        president_ecolo = "Hollande"
+    if indice_ecologie_president == 5:
+        president_ecolo = "Macron"
+    if indice_ecologie_president == 6 or indice_ecologie_president == 7:
+        president_ecolo = "Mitterrand"
+    if indice_ecologie_president == 8:
+        president_ecolo = "Sarkozy"
+
+
+    value_end_menu=1
+    while value_end_menu==1:
+        print("--- Menu ---\n\n"
+              "-si vous souhaitez afficher les mots les moins importants, tappez 1\n"
+              "-si vous souhaitez afficher les mots les plus importants, tappez 2\n"
+              "-si vous souhaitez afficher le mot le plus important par Chirac, tappez 3\n"
+              "-si vous souhaitez afficher les présidents parlant de Nation, ainsi que celui qui en parle le plus, tappez 4\n"
+              "-si vous souhaitez afficher le président parlant en premier de l'écologie, tappez 5\n"
+              "-si vous souhaitez afficher tous les mots de tous les discours hormis les non-importants, tappez 6\n")
+
+        value_menu=0
+        while value_menu<1 or value_menu>6:
+            value_menu=int(input())
+
+        if value_menu==1:
+            print("-Voici les mots les moins importants")
+            for i in range (len(mot_pas_important)):
+                print(mot_pas_important[i])
+        elif value_menu==2:
+            print("-Voici le mot le plus important du dossier :",mot_import_dossier)
+        elif value_menu == 3:
+            print("-Voici le mot le plus important des discours de Chirac :",mot_import_chirac)
+        elif value_menu == 4:
+            print("-Voici les présidents parlant de Nation")
+            for i in range(len(president_nation)):
+                print(president_nation[i])
+            print("-",presi_nation,"parle le plus de Nation")
+        elif value_menu == 5:
+            print("-",president_ecolo, "est le président qui parle le plus d'écologie")
+        elif value_menu == 6:
+            print("-Voici tous les mots de tous les discours hormis les non-importants")
+            for i in range (len(mot_sans_non_important)):
+                print(mot_sans_non_important[i])
+
+        value_end_menu=int(input("-Voulez vous acceder de nouveau au menu, tappez 1 sinon 2\n"))
+
+    print("Fin de l'execution du programme")
---
+++ function.py
@@ -0,0 +1,218 @@
+import os  # utilisé pour naviguer dans l'os
+import random # importer une possibilité de faire une fonction aléatoire
+
+# fonction qui permet de récuperer le nom des fichiers d'un dossier donné
+def list_of_files(directory, extension): # entre en paramètre le nom du dossier et le type de fichier à récupérer
+
+    files_names = [] # création d'une liste pour stocker les noms
+
+    for filename in os.listdir(directory): # récupère la liste des éléments dans le fichier donné en paramètre
+        if filename.endswith(extension): # entre dans la contidition que si on trouve le type de fichier donné en paramètre
+            files_names.append(filename) # ajout de chaque nom de fichier la liste créer au préalable
+    return files_names # on retourne la fonction pour la renvoyer
+
+
+
+#fonction qui permet d'associer le nom des présidents avec leur prenom
+def associer_prenom_president(nom_president):
+
+    prenoms_presidents = {
+        "Chirac ": " Jacques ",
+        " Giscrad d Estaing ": " Valéry ",
+        " Mitterrand ": " François ",
+        " Macron ": " Emmanuel ",
+        " Sarkozy ": " Nicolas "
+    }
+
+    return prenoms_presidents.get(nom_president, " Inconnu ")
+
+
+# fonction qui permet de prendre la liste des fichiers afin d'extraire seulement le nom des présidents stockés dans une liste
+def new_list(l): # entre en paramètre la liste à traiter
+
+    liste = [] # création d'une nouvelle liste
+
+    for i in range(len(l)): # parcour toute la liste donné en paramètre
+        val = l[i][11:-4] # enlève le format du fichier et le "Nomination_"
+        for j in range (len(val)): # parcour la chaine str des noms de fichier de cette liste
+            if val[j] == '1' or val[j] == '2': # entre dans la condition s'il y a un 1 ou un 2 dans la chaine
+                val = val[:-1] # enlève ces derniers à la dernière position
+        liste.append(val) # ajoute ces nouveaux noms la liste créée au préalable
+
+    return liste # renvoie la liste
+
+
+# fonction qui permet d'enlever les doublons dans une liste
+def supp_doublons(l): # entre en paramètre la liste à traiter
+
+    l = list(set(l)) # parcour la liste pour enlever les doublons
+
+    return l # renvoie la liste
+
+
+# fonction qui prend une chaine str comprenant des majuscules et renvoit une chaine str tout en minuscule
+def minuscule(txt): # entre en paramètre la chaine str à traiter
+
+    txt = list(txt) # met tous les caractères de la chaine dans une liste
+    for i in range (len(txt)): # parcour toute la liste
+        if ord(txt[i]) >= ord("A") and ord(txt[i]) <= ord("Z"): # entre dans la condition si un caractere est compris entre le code ascii de A et du Z
+            txt[i] = chr(ord(txt[i]) + ord("a")-ord("A")) # change cette majuscule en minuscule
+
+    return "".join(txt) # renvoie la liste en texte
+
+
+# fonction qui permet d'enlever la ponctuation d'une chaine str
+def ponctuation_str(l) : # entre en paramètre une chaine str
+
+    list_ponctuation = ["!","+","#",".",",","?",";",":","(",")",":","=","`",'"'] # création d'une liste comprenant toute les ponctuations
+    list_ponctuation_special = ["'","-"]# création d'une liste comprenant toute les ponctuations spéciales
+    list = [] # création d'une liste pour manipuler la chaine str
+    chaine = "" # création d'une chaine str
+
+    for i in range (len(l)) : # parcour toute la chaine str entré en paramètre
+        list.append(l[i]) # ajoute chaque caractère dans la liste créée au préalable
+        if list[i] in list_ponctuation_special : # entre dans la condition si le caractère est dans la liste de ponctuations spéciales
+            list[i] = " " # remplace le caractère par un espace
+        if list[i] in list_ponctuation : # entre dans la condition si le caractère est dans la liste de ponctuations
+            list[i] = "" # supprime le caractère
+        chaine += list[i] # ajoute chaque caractère dans la chaine str créée au préalable
+    return chaine # renvoie la chaine
+
+
+def cleaned(l): # entre en paramètre une liste comprenant le nom de fichier
+
+    os.mkdir('./cleaned/') # création d'un dossier "cleaned"
+    txt=""
+
+    for i in range (len(l)): # parcour la liste donné en paramètre
+        nom_fichier = l[i] # stocke le nom du fichier dans une variable
+        with open('./speeches/'+nom_fichier,"r") as f: # ouvre le fichier présent dans la liste
+            contenu = f.readlines() # stocke le contenu du fichier
+            for j in range(len(contenu)):
+                txt+=contenu[j]    # on ajoute a chaque fois le contenu a txt
+            txt = ponctuation_str(minuscule(txt))# appelle la fonction ponctuation_str et minuscule pour le contenu du fichier
+            nom_fichier_cleaned = "cleaned_" + l[i] # modifie le nom du fichier initial afin de ne pas les confondre
+            with open('./cleaned/' + nom_fichier_cleaned, 'w') as p: # crée le fichier à partir du nom du fichier modifié
+                p.write(txt) # écrit dans ce fichier le contenu du fichier iniatial modifié par la fonction ponctuation_str et minuscule
+
+
+def scan_ligne(chaine):
+
+    dico={}   # on définit un dictionnaire vide
+    mot=""   # on crée une variable mot où l'on stocke une chaine vide
+
+    for i in range (len(chaine)):  # on crée une boucle pour qui se répète jusqu'a la taille de la chaine
+        if chaine[i]!=" ":   # si le caractère est vide on l'ajoute a la variable mot
+            mot+=chaine[i]
+        else:
+            if mot=="d" or mot=="j" or mot=="s" or mot=="n":   # sinon pour les lettres d, j, s, n on ajoute un e après
+                mot+="e"
+            if mot=="c" :  # si c'est un c on ajoute un ela
+                mot+="ela"
+            if mot=="l": # si c'est un l on a une chance sur deux qu'il y est un a ou un e
+                a_ou_e = random.randint(1,2)
+                if a_ou_e == 1:
+                    mot +="e"
+                else:
+                    mot +="a"
+            else:
+                if mot in dico:    # si le contenu de la variable mot est dans le dictionnaire on ajoute 1
+                    dico[mot]+=1
+                else:
+                    dico[mot]=1
+            mot=""
+
+    return dico
+
+#fonction dictionnaire mot/nbr iteration
+
+def cpt_word(directory):
+
+    dictionnaire={}    # on crée un dictionnaire vide
+    texte=""  # Initialise une chaîne de caractères vide qui sera utilisée pour stocker le contenu combiné de tous les fichiers
+
+    list_name=list_of_files(directory,"txt")   #Appelle une fonction hypothétique list_of_files pour obtenir une liste de noms de fichiers se terminant par ".txt" dans le répertoire spécifié.
+    for i in range (len(list_name)):   # parcourt chaque élément de la liste des noms de fichiers.
+        loc_fichier="./"+str(directory)+"/"+list_name[i]
+        with open (loc_fichier,"r") as f:  # Ouvre le fichier en mode lecture.
+            ligne=f.readline()
+            while ligne!="":     # lit chaque ligne du fichier et ajoute son contenu à la chaîne
+                ligne=ligne.replace("\n","")   # supprime le caractère \n de chaque ligne par un vide
+                texte+=ligne+" "   # ajoute chaque ligne a la variable texte
+                ligne = f.readline()
+        dictionnaire = scan_ligne(texte)    #Appelle une fonction hypothétique scan_ligne qui compte les occurrences de chaque mot dans la ligne et met à jour le dictionnaire.
+
+    return dictionnaire
+
+
+
+import math
+
+# fonction mot-->score idf
+
+def score_idf_dico(dictionnaire):
+
+    nbr_mot = 0    # initialise une variable a 0
+    dico_idf = {}   # crée un dictionnaire vide
+    for iterations in dictionnaire.values():   #parcourt les valeurs du dictionnaire
+
+        nbr_mot += iterations
+
+    for mot in dictionnaire.keys():    # parcourt les clés du dictionnaire et effectue le calcul du score IDF pour chaque mot.
+        nbr_iteration = dictionnaire[mot]   # Récupère le nombre d'occurrences du mot dans le dictionnaire.
+        idf = math.log(1 / (nbr_iteration / nbr_mot))   # Calcule le score IDF pour le mot
+
+        dico_idf[mot] = idf   # stocke le score idf dans le dictionnaire
+
+    return dico_idf
+
+
+
+#fonction qui prend un dossier et retourne matrice tf idf en fonction de chaque fichier
+def matrice_tfidf(directory):
+    dico_TFIDF = {}       # création de dictionnaires vides
+    dico_TF = {}
+
+    List=[]         # création de liste vide
+
+    list_name = list_of_files(directory, "txt")
+    list_name=sorted(list_name)
+    List.append("mot")
+
+    dico_IDF=score_idf_dico(cpt_word(directory))           # Calcul du score IDF pour chaque mot dans le répertoire
+    all_word = cpt_word(directory)                  # Compte le nombre d'occurrences de chaque mot dans l'ensemble des documents
+
+    if "" in all_word:                  # Supprime la clé vide s'il y en a une
+        del all_word[""]
+
+    matrice = []                # Initialisation d'une matrice pour stocker les valeurs TF-IDF
+    nbr_ligne = int(len(list_name)) + 1     # Nombre de lignes dans la matrice (nombre de mots + 1 pour les titres)
+    for l in range(len(all_word) + 1):      # Crée une matrice remplie de zéros
+        ligne = ['0'] * nbr_ligne
+        matrice.append(ligne)
+    cpt = 1
+
+    for word in all_word.keys():     # Remplissage de la première colonne de la matrice avec les mots
+        matrice[cpt][0] = word
+        cpt += 1
+    for i in range(len(list_name)):             # Boucle sur les fichiers pour remplir la matrice avec les valeurs TF-IDF
+
+        loc_fichier = "./" + str(directory) + "/" + list_name[i]    # Suit le Chemin du fichier
+        with open(loc_fichier, "r") as f:
+            ligneTF =f.read()
+            ligneTF = ligneTF.replace("\n", "")
+            dico_TF=scan_ligne(ligneTF)             # Calcul des valeurs TF-IDF pour chaque mot dans le fichier
+            for mots in dico_TF.keys():
+                if mots in dico_IDF:
+                    dico_TFIDF[mots] = dico_IDF[mots]*dico_TF[mots]
+
+            matrice[0][i+1]=list_name[i]                # Remplissage de la première ligne de la matrice avec les noms de fichiers
+
+            for h in range(len(matrice)):           # Remplissage de la matrice avec les valeurs TF-IDF
+
+                for mot in dico_TFIDF.keys():
+                    if mot == matrice[h][0]:
+
+                        matrice[h][i+1]=dico_TFIDF[mot]
+
+    return matrice
